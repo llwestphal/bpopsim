16e8dd03106ccbe7103ecd911268ea8f72689c18
dittmarw <dittmarw@dittmarw-laptop>
2010-07-26 23:37:22 -0400

    header files

---
+++ src/c/bpopsim/pop_sim_header.h
@@ -0,0 +1,22 @@
+#ifndef POP_SIM_HEADER_H
+#define POP_SIM_HEADER_H
+
+/*pop_sim_header.h*/
+#include <stdint.h>
+
+double returnExp(double a);
+uint64_t returnBin(const uint64_t n, const double p);
+
+// NOTE: The BOOST Poisson random number generator DOES NOT WORK for large means!!!
+
+struct Individual {
+	double w;
+	double n;
+	char color;
+};
+
+double returnPoisson(const double n, const double p);
+
+int cline(int argc, char* argv[]);
+
+#endif

93f8cef892903bcef1d70d79ff491359aa8f31e4
Austin Meyer <Austin Meyer@localhost>
2011-07-08 11:50:43 -0500

    Cull populations in tree is activated.

--- src/c/bpopsim/main.cpp
+++ src/c/bpopsim/main.cpp
@@ -179,7 +179,7 @@ int main(int argc, char* argv[])
             population.Deterministic_Resample();
           else {
             population.Resample();
-            //population.CullPopulations();
+            population.CullPopulations();
           }
           
           if( print_single_fit ) {

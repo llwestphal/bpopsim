2c3e413c8154941c8e27807e0e02476d4e1625ab
Austin Meyer <Austin Meyer@localhost>
2011-08-06 15:51:13 -0500

    Integrated anyoption.  Created namespace for bpopsim, but have not started using anyoption yet.

--- src/c/bpopsim/anyoption.h
+++ src/c/bpopsim/anyoption.h
@@ -28,73 +28,77 @@
 
 using namespace std;
 
-namespace breseq {
-
+namespace bpopsim {
+  
+  inline vector<string> split(const  string  & theString,
+                              const  string  & theDelimiter
+                              ); 
+  
 	class AnyOption
 	{
-
+    
 	public: /* the public interface */
 		AnyOption();
 		AnyOption(const string& usage_prefix);
 		AnyOption(int maxoptions );
 		AnyOption(int maxoptions , int maxcharoptions);
 		~AnyOption();
-
+    
 		/*
-			 * following set methods specifies the
+     * following set methods specifies the
 		 * special characters and delimiters
 		 * if not set traditional defaults will be used
-			 */
-
+     */
+    
 		void setCommandPrefixChar( char _prefix );   /* '-' in "-w" */
 		void setCommandLongPrefix( char *_prefix );  /* '--' in "--width" */
 		void setFileCommentChar( char _comment );    /* '#' in shellscripts */
 		void setFileDelimiterChar( char _delimiter );/* ':' in "width : 100" */
-
+    
 		/*
 		 * provide the input for the options
-			 * like argv[] for commndline and the
-			 * option file name  to use;
+     * like argv[] for commndline and the
+     * option file name  to use;
 		 */
-
+    
 		void useCommandArgs( int _argc, char** _argv );
 		void useFilename( const char *_filename );
-
+    
 		/*
-			 * turn off the POSIX style options
-			 * this means anything starting with a '-' or "--"
-			 * will be considered a valid option
-			 * which alo means you cannot add a bunch of
-			 * POIX options chars together like "-lr"  for "-l -r"
-			 *
-			 */
-
+     * turn off the POSIX style options
+     * this means anything starting with a '-' or "--"
+     * will be considered a valid option
+     * which alo means you cannot add a bunch of
+     * POIX options chars together like "-lr"  for "-l -r"
+     *
+     */
+    
 		void noPOSIX();
-
+    
 		/*
-			 * prints warning verbose if you set anything wrong
-			 */
+     * prints warning verbose if you set anything wrong
+     */
 		void setVerbose();
-
-
+    
+    
 		/*
-			 * there are two types of options
-			 *
-			 * Option - has an associated value ( -w 100 )
-			 * Flag  - no value, just a boolean flag  ( -nogui )
-			 *
+     * there are two types of options
+     *
+     * Option - has an associated value ( -w 100 )
+     * Flag  - no value, just a boolean flag  ( -nogui )
+     *
 		 * the options can be either a string ( GNU style )
-			 * or a character ( traditional POSIX style )
-			 * or both ( --width, -w )
-			 *
-			 * the options can be common to the commandline and
-			 * the optionfile, or can belong only to either of
-			 * commandline and optionfile
-			 *
-			 * following set methods, handle all the aboove
+     * or a character ( traditional POSIX style )
+     * or both ( --width, -w )
+     *
+     * the options can be common to the commandline and
+     * the optionfile, or can belong only to either of
+     * commandline and optionfile
+     *
+     * following set methods, handle all the aboove
 		 * cases of options.
-			 */
-
+     */
+    
 		/* options comman to command line and option file */
 		void setOption( const char *opt_string );
 		void setOption( char  opt_char );
@@ -102,7 +106,7 @@ namespace breseq {
 		void setFlag( const char *opt_string );
 		void setFlag( char  opt_char );
 		void setFlag( const char *opt_string , char opt_char );
-
+    
 		/* options read from commandline only */
 		void setCommandOption( const char *opt_string );
 		void setCommandOption( char  opt_char );
@@ -110,7 +114,7 @@ namespace breseq {
 		void setCommandFlag( const char *opt_string );
 		void setCommandFlag( char  opt_char );
 		void setCommandFlag( const char *opt_string , char opt_char );
-
+    
 		/* options read from an option file only  */
 		void setFileOption( const char *opt_string );
 		void setFileOption( char  opt_char );
@@ -118,32 +122,32 @@ namespace breseq {
 		void setFileFlag( const char *opt_string );
 		void setFileFlag( char  opt_char );
 		void setFileFlag( const char *opt_string , char opt_char );
-
+    
 		/*
-			 * process the options, registerd using
-			 * useCommandArgs() and useFileName();
-			 */
+     * process the options, registerd using
+     * useCommandArgs() and useFileName();
+     */
 		void processOptions();
 		void processCommandArgs();
 		void processCommandArgs( int max_args );
 		bool processFile();
-
+    
 		/*
-			 * process the specified options
-			 */
+     * process the specified options
+     */
 		void processCommandArgs( int _argc, char** _argv );
 		void processCommandArgs( int _argc, char** _argv, int max_args );
 		bool processFile( const char *_filename );
-
+    
 		/*
-			 * get the value of the options
+     * get the value of the options
 		 * will return NULL if no value is set
-			 */
+     */
 		string* getValue( const string option );
 		bool  getFlag( const string option );
 		string* getValue( char _optchar );
 		bool  getFlag( char _optchar );
-
+    
 		/*
 		 * Print Usage
 		 */
@@ -151,25 +155,25 @@ namespace breseq {
 		void printAutoUsage();
 		void addUsage( string line );
 		void printHelp();
-			/* print auto usage printing for unknown options or flag */
+    /* print auto usage printing for unknown options or flag */
 		void autoUsagePrint(bool flag);
-
+    
 		/*
-			 * get the argument count and arguments sans the options
-			 */
+     * get the argument count and arguments sans the options
+     */
 		int   getArgc();
 		char* getArgv( int index );
 		bool  hasOptions();
-
-
-
+    
+    
+    
 	private:
 		string word_wrap(string sentence, int width);
 		template<class T> void addOptionOrFlag(const string& option_name, const string& option_description, const T& option_default_value, bool has_argument, bool has_default_value)
 		{
       // split the name first 
       vector<string> option_name_split = split(option_name, ",");
-
+      
       // assign default value to both long and short option name variants
       if (has_default_value) 
       {
@@ -195,10 +199,10 @@ namespace breseq {
 					this->setOption(option_name_split[0].c_str());
 				else
 					this->setFlag(option_name_split[0].c_str());
-
+        
         usage = "  [ " + option_name_split[0] + " ]";
 			}
-
+      
 			if (has_argument) usage += " arg";
 			if (has_default_value)
 			{
@@ -208,10 +212,10 @@ namespace breseq {
 				else
 					usage += " (optional)";
 			}
-
+      
 			if (usage.size() < USAGE_LEFT_COLUMN_WIDTH)
 				usage += string(abs(static_cast<int32_t>(USAGE_LEFT_COLUMN_WIDTH - usage.size())), ' ');
-
+      
 			string wrapped_description = word_wrap(option_description, terminal_width - USAGE_LEFT_COLUMN_WIDTH);
 			string search_string = "\n";
 			string replace_string = search_string + string(USAGE_LEFT_COLUMN_WIDTH, ' ');
@@ -221,10 +225,10 @@ namespace breseq {
 				pos += replace_string.size();
 			}
 			usage += wrapped_description;
-
+      
 			addUsage(usage);
 		}
-
+    
 	public:
 		AnyOption& operator()(const string& option_name, const string& option_description)
 		{
@@ -248,45 +252,45 @@ namespace breseq {
 			addOptionOrFlag(option_name, option_description, option_default_value, true, true);
 			return *this;
 		}
-
+    
 		string operator[](const string& option_name);
 		bool count(const string& option_name);
-
-
-
+    
+    
+    
 	private: /* the hidden data structure */
 		int argc;		/* commandline arg count  */
 		char** argv;  		/* commndline args */
 		const char* filename; 	/* the option file */
 		char* appname; 	/* the application name from argv[0] */
-
+    
 		int *new_argv; 		/* arguments sans options (index to argv) */
 		int new_argc;   	/* argument count sans the options */
 		int max_legal_args; 	/* ignore extra arguments */
-
-
+    
+    
 		/* option strings storage + indexing */
 		int max_options; 	/* maximum number of options */
 		vector<string> options; 	/* storage */
 		int *optiontype; 	/* type - common, command, file */
 		int *optionindex;	/* index into value storage */
 		int option_counter; 	/* counter for added options  */
-
+    
 		/* option chars storage + indexing */
 		int max_char_options; 	/* maximum number options */
 		char *optionchars; 	/*  storage */
 		int *optchartype; 	/* type - common, command, file */
 		int *optcharindex; 	/* index into value storage */
 		int optchar_counter; 	/* counter for added options  */
-
+    
 		/* values */
 		map <int, string> values; 		/* common value storage */
 		int g_value_counter; 	/* globally updated value index LAME! */
-
+    
 		/* help and usage */
 		vector<string> usage; 	/* usage */
 		int usage_lines;	/* number of usage lines */
-
+    
 		bool command_set;	/* if argc/argv were provided */
 		bool file_set;		/* if a filename was provided */
 		bool mem_allocated;     /* if memory allocated in init() */
@@ -294,7 +298,7 @@ namespace breseq {
 		bool verbose;		/* silent|verbose */
 		bool print_usage;	/* usage verbose */
 		bool print_help;	/* help verbose */
-
+    
 		char opt_prefix_char;		/*  '-' in "-w" */
 		char long_opt_prefix[MAX_LONG_PREFIX_LENGTH+1]; /* '--' in "--width" */
 		char file_delimiter_char;	/* ':' in width : 100 */
@@ -305,37 +309,37 @@ namespace breseq {
 		char endofline;
 		char whitespace;
 		char nullterminate;
-
+    
 		bool set;   //was static member
 		bool once;  //was static member
-
+    
 		bool hasoptions;
 		bool autousage;
-
-
-
+    
+    
+    
 		int terminal_width;
 		map<string, string> default_values;
-
-
-
+    
+    
+    
 	private: /* the hidden utils */
 		void init();
 		void init(int maxopt, int maxcharopt );
 		bool alloc();
 		void cleanup();
 		bool valueStoreOK();
-
+    
 		/* grow storage arrays as required */
 		bool doubleOptStorage();
 		bool doubleCharStorage();
 		bool doubleUsageStorage();
-
+    
 		bool setValue( const string option , string value );
 		bool setFlagOn( const string option );
 		bool setValue( char optchar , string value);
 		bool setFlagOn( char optchar );
-
+    
 		void addOption( const string option , int type );
 		void addOption( char optchar , int type );
 		void addOptionError( const string opt);
@@ -345,12 +349,12 @@ namespace breseq {
 		bool CommandSet();
 		bool FileSet();
 		bool POSIX();
-
+    
 		char parsePOSIX( char* arg );
 		int parseGNU( char *arg );
 		bool matchChar( char c );
 		int matchOpt( char *opt );
-
+    
 		/* dot file methods */
 		char *readFile();
 		char *readFile( const char* fname );
@@ -359,13 +363,13 @@ namespace breseq {
 		char *chomp( char *str );
 		void valuePairs( char *type, string value );
 		void justValue( char *type );
-
+    
 		void printVerbose( const char *msg );
 		void printVerbose( char *msg );
 		void printVerbose( char ch );
 		void printVerbose( );
-
+    
 	}; // class AnyOption
-
+  
 } // namespace breseq
 #endif /* ! _ANYOPTION_H */
